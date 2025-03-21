45d85ff4317bd956ffa3bb1e886d3026e46f757f
Suraj  Sonkar <surajvirendrasonkar@gmail.com>
2025-03-21 18:21:22 +0530

    all the routes are created

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { assets } from '../assets/assets';
+const Navbar = () => {
+	return (
+		<div className="flex items-center justify-between py-5 font-medium">
+			<img src={assets.logo} className="w-36"></img>
+		</div>
+	);
+};
+
+export default Navbar;
